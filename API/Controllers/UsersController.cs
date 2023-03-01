9a1b94648360a63e0cc7b3eb689c0b44c0dd306d
Daniel Madriz <almak_-_@hotmail.com>
2023-02-28 22:23:35 -0600

    End of section 2

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,34 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            this._context = context;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            var users = await _context.Users.ToList();
+            
+            return users;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            return await _context.Users.Find(id);
+        }
+    }
+}
+//git token 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
